a6b11aef69d940f5f421d67e8fc05f06d3724352
Yao Yao <yaoy.cs@gmail.com>
2019-04-26 17:14:16 -0700

    added a new variable for easier configuration

--- cerenkov3_data/edge_4DGenome.sh
+++ cerenkov3_data/edge_4DGenome.sh
@@ -6,12 +6,16 @@ mv 4DGenome_HomoSapiens_hg19.txt ./edge/snp-gene/Chromatin-Interaction/4DGenome/
 Rscript edge_4DGenome_preprocess.R
 Rscript edge_4DGenome_make_BED.R
 
+# Run bedtools intersect
+
+## Modify this path if you have your own SNP bed file 
+snp_bed="./vertex/SNP/OSU18/osu18_SNP.bed"
+
 fd_genome_dir="./edge/snp-gene/Chromatin-Interaction/4DGenome"
-osu18_snp_dir="./vertex/SNP/OSU18"
 ensembl_dir="./vertex/gene/Ensembl"
 
-bedtools intersect -a ${fd_genome_dir}/4DGenome_InteractorA.bed -b ${osu18_snp_dir}/osu18_SNP.bed -wo > ${fd_genome_dir}/InteractorA_SNP.bed
-bedtools intersect -a ${fd_genome_dir}/4DGenome_InteractorB.bed -b ${osu18_snp_dir}/osu18_SNP.bed -wo > ${fd_genome_dir}/InteractorB_SNP.bed
+bedtools intersect -a ${fd_genome_dir}/4DGenome_InteractorA.bed -b ${snp_bed} -wo > ${fd_genome_dir}/InteractorA_SNP.bed
+bedtools intersect -a ${fd_genome_dir}/4DGenome_InteractorB.bed -b ${snp_bed} -wo > ${fd_genome_dir}/InteractorB_SNP.bed
 
 bedtools intersect -a ${fd_genome_dir}/4DGenome_InteractorA.bed -b ${ensembl_dir}/ensembl_gene_TSS.bed -wo > ${fd_genome_dir}/InteractorA_TSS.bed
 bedtools intersect -a ${fd_genome_dir}/4DGenome_InteractorB.bed -b ${ensembl_dir}/ensembl_gene_TSS.bed -wo > ${fd_genome_dir}/InteractorB_TSS.bed
