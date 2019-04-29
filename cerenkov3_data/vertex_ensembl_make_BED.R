c9d5088e87de83623dc08480516bb88bcc562a34
Yao Yao <yaoy.cs@gmail.com>
2019-04-29 15:06:28 -0700

    modified comments; specified col_types when reading tsv

--- cerenkov3_data/vertex_ensembl_make_BED.R
+++ cerenkov3_data/vertex_ensembl_make_BED.R
@@ -1,6 +1,6 @@
 # This script aims to:
     # 1. use `p1_ensembl_gene_df.tsv` as input
-    # 2. save "chromosome", "promoter_start", "promoter_end", "gene_name", "ensembl_gene_id" and "strand" to a BED file, `ensembl_gene_promoter.bed`
+    # 2. save "chromosome", "promoter_start", "promoter_end", "gene_name", "ensembl_gene_id" and "strand" to a BED file
     #     - You always have `transcript_start < transcript_end`, `gene_start < gene_end` and `TSS_start + 1 == TSS_end`
     #     - If strand == +1, define:
     #         - promoter_start = TSS_start - 2000, and 
@@ -8,8 +8,8 @@
     #     - If strand == -1, define:
     #         - promoter_start = TSS_start - 500, and 
     #         - promoter_end = TSS_end + 2000 (bigger coordinates mean upstream)
-    # 3. save "chromosome", "TSS_start", "TSS_end", "gene_name", "ensembl_gene_id" and "strand" to a BED file, `ensembl_gene_TSS.bed`
-    # 4. Note: BED format is not strictly followed here.
+    # 3. save "chromosome", "TSS_start", "TSS_end", "gene_name", "ensembl_gene_id" and "strand" to a BED file
+# Note: BED format is not strictly followed here.
 
 library(dplyr)
 library(readr)
@@ -17,8 +17,20 @@ library(magrittr)
 library(tidyr)
 
 ensembl_dir <- "./vertex/gene/Ensembl/"
-
-ensembl_df <- read_tsv(paste(ensembl_dir, "p1_Ensembl.tsv", sep=""))
+# Note that this the coordinates in this file is already 0-based (converted by vertex_ensembl_preprocess.R) and compatible with BED format
+ensembl_df <- read_tsv(paste(ensembl_dir, "p1_Ensembl.tsv", sep=""), 
+					   col_types=cols(chromosome = col_character(),
+								      transcript_start = col_integer(),
+									  transcript_end = col_integer(),
+									  strand = col_integer(),
+									  ensembl_gene_id = col_character(),
+									  gene_name = col_character(),
+									  gene_start = col_integer(),
+									  gene_end = col_integer(),
+									  transcript_id = col_character(),
+									  transcript_name = col_character(),
+									  TSS_start = col_integer(),
+									  TSS_end = col_integer()))
 
 ensembl_df %>% 
 	mutate(promoter_start = case_when(strand == 1 ~ TSS_start - 2000, 
@@ -36,11 +48,11 @@ ensembl_df %>%
 	write.table(paste(ensembl_dir, "p2_Ensembl_TSS.bed", sep=""), sep = "\t", 
 				quote = FALSE, row.names = FALSE, col.names = FALSE)
 
-## save "chromosome", "transcript_start", "transcript_end", "gene_name", "ensembl_gene_id" and "strand" to a BED file, `ensembl_gene_transcript.bed`
+# 4. save "chromosome", "transcript_start", "transcript_end", "gene_name", "ensembl_gene_id" and "strand" to a BED file
 # ensembl_df %>% 
 # 	select(chromosome, transcript_start, transcript_end, gene_name, ensembl_gene_id, strand) %>%
 # 	distinct(.keep_all = TRUE) %>% 
-# 	write.table("p2_Ensembl_transcript.bed", sep = "\t", 
+# 	write.table(paste(ensembl_dir, "p2_Ensembl_transcript.bed", sep=""), sep = "\t", 
 # 				quote = FALSE, row.names = FALSE, col.names = FALSE)
 
 ## save "chromosome", "gene_start", "gene_end", "gene_name", "ensembl_gene_id" and "strand" to a BED file, `ensembl_gene_itself.bed`
