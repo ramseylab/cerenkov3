086b6a4ae7186d8581aa547ddfc90e38f458da29
Yao Yao <yaoy.cs@gmail.com>
2019-04-30 12:59:31 -0700

    added command to remove unnecessary resources

--- cerenkov3_data/edge_coexpedia.sh
+++ cerenkov3_data/edge_coexpedia.sh
@@ -2,6 +2,7 @@
 
 wget "http://www.coexpedia.org/dump/human_cx_net.zip"
 unzip -q human_cx_net.zip -d ./edge/gene-gene/co-expression/Coexpedia/
-mv human_cx_net.zip ./edge/gene-gene/co-expression/Coexpedia/
+# mv human_cx_net.zip ./edge/gene-gene/co-expression/Coexpedia/
+rm human_cx_net.zip
 
 python3 edge_coexpedia.py
