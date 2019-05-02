d1c3486d9c510b984aa330f4ab538431c7ae754a
Yao Yao <yaoy.cs@gmail.com>
2019-05-02 15:31:31 -0700

    suppressed downloading if file exists; added progress messages

--- cerenkov3_data/edge_coexpedia.sh
+++ cerenkov3_data/edge_coexpedia.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-wget "http://www.coexpedia.org/dump/human_cx_net.zip"
-unzip -q human_cx_net.zip -d ./resource/Coexpedia/
-rm human_cx_net.zip
+DIRECTORY="./resource/Coexpedia"
+
+if [ -d "${DIRECTORY}/Hsa" ]; then
+    # Control will enter here if that folder exists.
+    echo "[Coexpedia] folder '${DIRECTORY}/Hsa' exists; no downloading"
+else
+    wget "http://www.coexpedia.org/dump/human_cx_net.zip"
+    unzip -q human_cx_net.zip -d ${DIRECTORY}
+    rm human_cx_net.zip
+fi
 
 python3 edge_coexpedia.py
+
+echo "[Coexpedia] done!"
--- cerenkov3_data/edge_humannet.sh
+++ cerenkov3_data/edge_humannet.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-wget "https://www.inetbio.org/humannet/networks/HumanNet-XN.tsv"
+DIRECTORY="./resource/HumanNet"
 
-mv HumanNet-XN.tsv ./resource/HumanNet
+if [ -f "${DIRECTORY}/HumanNet-XN.tsv" ]; then
+    # Control will enter here if that file exists.
+    echo "[HumanNet] file '${DIRECTORY}/HumanNet-XN.tsv' exists; no downloading"
+else
+    wget "https://www.inetbio.org/humannet/networks/HumanNet-XN.tsv"
+    mv HumanNet-XN.tsv ${DIRECTORY}
+fi
 
 python3 edge_humannet.py
+
+echo "[HumanNet] done!"
