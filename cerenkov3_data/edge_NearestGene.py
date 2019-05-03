e8930758847b5cc792bef9114974bb6b73237158
Yao Yao <yaoy.cs@gmail.com>
2019-05-03 16:40:08 -0700

    changed filenames and output paths

--- cerenkov3_data/edge_NearestGene.py
+++ cerenkov3_data/edge_NearestGene.py
@@ -6,7 +6,8 @@ from util_path import get_path
 
 
 def make_snp_start_BED(snp_bed_fn):
-    snp_bed = pd.read_csv(snp_bed_fn, sep="\t", header=None, usecols=[0,1,2,3],
+    snp_bed = pd.read_csv(snp_bed_fn, sep="\t", header=None, usecols=[0,1,2,3], 
+                          dtype={"chromStart": int, "chromEnd": int},
                           names=["chrom", "chromStart", "chromEnd", "name"])
     snp_bed.loc[:, "chromEnd"] = snp_bed.loc[:, "chromEnd"] - 1
 
@@ -15,7 +16,8 @@ def make_snp_start_BED(snp_bed_fn):
 
 def make_TSS_start_BED(tss_bed_fn):
     tss_bed = pd.read_csv(tss_bed_fn, sep="\t", header=None, usecols=[0,1,2,3,4,5],
-                          dtype={"strand": int}, names=["chrom", "chromStart", "chromEnd", "name", "id", "strand"])
+                          dtype={"chromStart": int, "chromEnd": int, "strand": int}, 
+                          names=["chrom", "chromStart", "chromEnd", "name", "id", "strand"])
 
     # Determine TSS by strands
     fwd_strand = (tss_bed.loc[:, "strand"] == 1)
@@ -39,16 +41,15 @@ def convert_to_df(closest_obj):
 
 
 if __name__ == "__main__":
-    output_dir = get_path("edge/snp-gene/NearestGene")
+    snp_dir = get_path("vertex/SNP")
+    snp_fn = "osu18_SNP.bed"
+    snp_start_bed = make_snp_start_BED(os.path.join(snp_dir, snp_fn))
 
-    snp_dir = get_path("vertex/SNP/OSU18")
-    snp_start_bed = make_snp_start_BED(os.path.join(snp_dir, "osu18_SNP.bed"))
-
-    ensembl_dir = get_path("vertex/gene/Ensembl")
-    ensembl_TSS_start_bed = make_TSS_start_BED(os.path.join(ensembl_dir, "p2_Ensembl_TSS.bed"))
-
-    entrez_dir = get_path("vertex/gene/Entrez")
-    entrez_TSS_start_bed = make_TSS_start_BED(os.path.join(entrez_dir, "p3_Entrez_TSS.bed"))
+    gene_dir = get_path("vertex/gene")
+    ensembl_TSS_fn = "Ensembl_TSS.bed"
+    ensembl_TSS_start_bed = make_TSS_start_BED(os.path.join(gene_dir, ensembl_TSS_fn))
+    entrez_TSS_fn = "Entrez_TSS.bed"
+    entrez_TSS_start_bed = make_TSS_start_BED(os.path.join(gene_dir, entrez_TSS_fn))
     
     snp_start_bt = convert_to_bedtool_obj(snp_start_bed)
     ensembl_TSS_start_bt = convert_to_bedtool_obj(ensembl_TSS_start_bed)
@@ -60,18 +61,19 @@ if __name__ == "__main__":
     snp_ensembl_map = convert_to_df(snp_ensembl_closest)
     snp_entrez_map = convert_to_df(snp_entrez_closest)
 
-    snp_ensembl_map.to_csv(os.path.join(output_dir, "p1_SNP_x_NearestEnsembl.tsv"), sep="\t", header=True, index=False)
-    snp_entrez_map.to_csv(os.path.join(output_dir, "p1_SNP_x_NearestEntrez.tsv"), sep="\t", header=True, index=False)
+    res_dir = get_path("resource/NearestGene")
+    snp_ensembl_map.to_csv(os.path.join(res_dir, "p1_SNP_x_NearestEnsembl.tsv"), sep="\t", header=True, index=False)
+    snp_entrez_map.to_csv(os.path.join(res_dir, "p1_SNP_x_NearestEntrez.tsv"), sep="\t", header=True, index=False)
 
     # For any closest SNP-gene pair (s,g), if g's Ensembl ID can be mapped to an Entrez ID, then output an edge (s, g_entrez)
     # if not, output 2 edges in the edgelist file, one (s, g_ensembl) and the other (s, g_entrez)
-    e2e_df = pd.read_csv(os.path.join(entrez_dir, "p2_Ensembl_x_Entrez.tsv"), sep="\t")
+    e2e_df = pd.read_csv(os.path.join(gene_dir, "Ensembl_x_Entrez.tsv"), sep="\t")
     
     snp_ensembl_el = snp_ensembl_map.loc[:, ["snpName", "geneID"]]
     snp_ensembl_el = snp_ensembl_el.loc[~snp_ensembl_el.geneID.isin(set(e2e_df.Ensembl_Gene_ID)), :]  # Keep Non-mappable Ensembl IDs only
-    
     snp_entrez_el = snp_entrez_map.loc[:, ["snpName", "geneID"]]
-    
     snp_gene_el = pd.concat([snp_ensembl_el, snp_entrez_el])
-    snp_gene_el.to_csv(os.path.join(output_dir, "p2_SNP_x_NearestGene.edgelist"), sep="\t", header=False, index=False)
+
+    output_dir = get_path("edge/snp-gene")
+    snp_gene_el.to_csv(os.path.join(output_dir, "SNP_x_NearestGene.edgelist"), sep="\t", header=False, index=False)
     
