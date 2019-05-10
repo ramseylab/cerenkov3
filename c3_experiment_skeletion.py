649f6b34e01e654e1d21f497df97eb7f32154e4d
Yao Yao <yaoy.cs@gmail.com>
2019-05-10 11:45:25 -0700

    refactored: integrated fixed CV

--- c3_experiment_skeletion.py
+++ c3_experiment_skeletion.py
@@ -10,7 +10,7 @@ from locus_sampling.cross_validation import BalancedGroupKFold
 _RANDOM_STATE = 1337
 
 
-def prepare_for_experiment():
+def prepare_for_experiment(fixed_cv=False):
     snp_feat_path = "./cerenkov3_data/vertex/SNP/osu18_cerenkov_feat_mat_plus_group_size.tsv"
     snp_id_path = "./cerenkov3_classifier/INT_ID_EDGELIST/SNP_INT_ID.tsv"
     snp_group_path = "./cerenkov3_data/vertex/SNP/osu18_groups.tsv"
@@ -54,18 +54,29 @@ def prepare_for_experiment():
 
     c3c = Cerenkov3Classifier(sgn_manager, n2v_manager, ext_manager, base_clf)
 
-    return X_INT_ID, y, g, c3c
+    if fixed_cv:
+        partition_table_path = "./cerenkov3_data/vertex/SNP/osu18_replications_fold_assignments.tsv"
+        partition_table = pd.read_csv(partition_table_path, sep="\t")
+        partition_table = Xyg.loc[:, ["name"]].merge(partition_table, how="inner", on="name")  # align partition_table to X
+
+        return X_INT_ID, y, g, c3c, partition_table
+    else:
+        return X_INT_ID, y, g, c3c, None
 
 
-def x_validate(sgn_weight_dist, n2v_param_dist, clf_param_dist, scoring, n_repeats=1):    
-    X_INT_ID, y, g, c3c = prepare_for_experiment()
+def x_validate(sgn_weight_dist, n2v_param_dist, clf_param_dist, scoring, n_repeats=1, n_splits=5, fixed_cv=False):    
+    X_INT_ID, y, g, c3c, partition_table = prepare_for_experiment(fixed_cv=fixed_cv)
 
     param_dist = {**sgn_weight_dist, **n2v_param_dist, **clf_param_dist}
     c3c = c3c.set_params(**param_dist)
 
     def _run():
         for i in range(0, n_repeats):
-            cv = BalancedGroupKFold(n_splits=5, slop_allowed=0.5, random_state=_RANDOM_STATE + i)
+            if fixed_cv:
+                cv = FixedReplicatedKFold(n_splits=n_splits, partition_table=partition_table, repli_colname="replication{}".format(i+1))
+            else:
+                cv = BalancedGroupKFold(n_splits=n_splits, slop_allowed=0.5, random_state=_RANDOM_STATE + i)
+            
             result_dict = cross_validate(estimator=c3c, X=X_INT_ID, y=y, groups=g, scoring=scoring, cv=cv, return_train_score=True)
             yield result_dict
         
@@ -73,51 +84,44 @@ def x_validate(sgn_weight_dist, n2v_param_dist, clf_param_dist, scoring, n_repea
 
     return result_dict_list
 
+def random_hp_search(sgn_weight_dist, n2v_param_dist, clf_param_dist, scoring, n_iter_search=None, n_repeats=1, n_splits=5, fixed_cv=False)
+    X_INT_ID, y, g, c3c, partition_table = prepare_for_experiment(fixed_cv=fixed_cv)
 
-# def hp_search(sgn_weight_dist, n2v_param_dist, clf_param_dist, search_type, scoring, n_iter_search=None):
-#     X_INT_ID, y, g, c3c = prepare_for_experiment()
-    
-#     param_dist = {**sgn_weight_dist, **n2v_param_dist, **clf_param_dist}
-    
-#     cv = BalancedGroupKFold(n_splits=5, slop_allowed=0.5, random_state=_RANDOM_STATE)
-
-#     if search_type == "random":
-#         search = RandomizedSearchCV(c3c, param_distributions=param_dist,
-#                                     n_iter=n_iter_search, cv=cv, scoring=scoring, random_state=_RANDOM_STATE, refit=False, return_train_score=True)
-#         search.fit(X_INT_ID, y, groups=g)
-#     elif search_type == "grid":
-#         search = GridSearchCV(c3c, param_grid=param_dist, cv=cv, scoring=scoring, refit=False, return_train_score=True)
-#         search.fit(X_INT_ID, y, groups=g)
-#     else:
-#         raise ValueError("Invalid search_type. Got '{}'".format(search_type))
-
-#     return search
-    
-def hp_search(sgn_weight_dist, n2v_param_dist, clf_param_dist, search_type, scoring, n_iter_search=None, n_repeats=1):
-    X_INT_ID, y, g, c3c = prepare_for_experiment()
-    
     param_dist = {**sgn_weight_dist, **n2v_param_dist, **clf_param_dist}
-    
+
     def _run():
-        if search_type == "random":
-            for i in range(0, n_repeats):
-                cv = BalancedGroupKFold(n_splits=5, slop_allowed=0.5, random_state=_RANDOM_STATE + i)
-                search = RandomizedSearchCV(c3c, param_distributions=param_dist,
-                                            n_iter=n_iter_search, cv=cv, scoring=scoring, random_state=_RANDOM_STATE, refit=False, return_train_score=True)
-                search.fit(X_INT_ID, y, groups=g)
-                yield search
-        elif search_type == "grid":
-            for i in range(0, n_repeats):
-                cv = BalancedGroupKFold(n_splits=5, slop_allowed=0.5, random_state=_RANDOM_STATE + i)
-                search = GridSearchCV(c3c, param_grid=param_dist, cv=cv, scoring=scoring, refit=False, return_train_score=True)
-                search.fit(X_INT_ID, y, groups=g)
-                yield search
-        else:
-            raise ValueError("Invalid search_type. Got '{}'".format(search_type))
+        for i in range(0, n_repeats):
+            if fixed_cv:
+                cv = FixedReplicatedKFold(n_splits=n_splits, partition_table=partition_table, repli_colname="replication{}".format(i+1))
+            else:
+                cv = BalancedGroupKFold(n_splits=n_splits, slop_allowed=0.5, random_state=_RANDOM_STATE + i)
+
+            search = RandomizedSearchCV(c3c, param_distributions=param_dist,
+                                        n_iter=n_iter_search, cv=cv, scoring=scoring, random_state=_RANDOM_STATE, refit=False, return_train_score=True)
+            search.fit(X_INT_ID, y, groups=g)
+            yield search
 
     search_list = list(_run())
 
     return search_list
 
-if __name__ == "__main__":
-    pass
+    
+def grid_hp_search(sgn_weight_dist, n2v_param_dist, clf_param_dist, scoring, n_repeats=1, n_splits=5, fixed_cv=False):
+    X_INT_ID, y, g, c3c, partition_table = prepare_for_experiment(fixed_cv=fixed_cv)
+    
+    param_dist = {**sgn_weight_dist, **n2v_param_dist, **clf_param_dist}
+    
+    def _run():
+        for i in range(0, n_repeats):
+            if fixed_cv:
+                cv = FixedReplicatedKFold(n_splits=n_splits, partition_table=partition_table, repli_colname="replication{}".format(i+1))
+            else:
+                cv = BalancedGroupKFold(n_splits=n_splits, slop_allowed=0.5, random_state=_RANDOM_STATE + i)
+
+            search = GridSearchCV(c3c, param_grid=param_dist, cv=cv, scoring=scoring, refit=False, return_train_score=True)
+            search.fit(X_INT_ID, y, groups=g)
+            yield search
+    
+    search_list = list(_run())
+
+    return search_list
