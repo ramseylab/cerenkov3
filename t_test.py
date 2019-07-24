98004244fa3864c3e73eb31fbafd84b258ad8073
Yao Yao <yaoy.cs@gmail.com>
2019-07-24 11:12:49 -0700

    added more tests

--- t_test.py
+++ t_test.py
@@ -25,20 +25,29 @@ def t_test_report(perf_df_a, tag_a, perf_df_b, tag_b, metric_cols):
 if __name__ == "__main__":
     metric_cols = ["test_AUPRC", "test_AUROC", "test_AVGRANK"]
 
+#     c1_n2v_perf = pd.read_csv("./experiment_result/c3_minus_LS_cross_validate_xv_report.tsv", sep="\t", usecols=metric_cols)
+
+    gwava_perf = pd.read_csv("./experiment_result/gwava_performance_CERENKOV2_1337.tsv", sep="\t", usecols=metric_cols)
     c1_perf = pd.read_csv("./experiment_result/c1_cross_validate_xv_report.tsv", sep="\t", usecols=metric_cols)
-    c1_n2v_perf = pd.read_csv("./experiment_result/c3_minus_LS_cross_validate_xv_report.tsv", sep="\t", usecols=metric_cols)
     c1_ls_perf = pd.read_csv("./experiment_result/c2_cross_validate_xv_report.tsv", sep="\t", usecols=metric_cols)
-    # C3 = C1 + LS + N2V
-    c1_ls_n2v_perf = pd.read_csv("./experiment_result/c3_cross_validate_xv_report.tsv", sep="\t", usecols=metric_cols)
+    c3_perf = pd.read_csv("./experiment_result/c3_cross_validate_xv_report.tsv", sep="\t", usecols=metric_cols)  # C3 = C1 + LS + N2V
 
-    report_list = list(t_test_report(c1_perf, "C1", c1_n2v_perf, "C1+N2V", metric_cols))
-    report_df_1 = pd.DataFrame(report_list)
+    c1_perf.loc[:, "test_AVGRANK"] = -1 * c1_perf.loc[:, "test_AVGRANK"]
+    c1_ls_perf.loc[:, "test_AVGRANK"] = -1 * c1_ls_perf.loc[:, "test_AVGRANK"]
+    c3_perf.loc[:, "test_AVGRANK"] = -1 * c3_perf.loc[:, "test_AVGRANK"]
 
-    report_list = list(t_test_report(c1_perf, "C1", c1_ls_perf, "C1+LS", metric_cols))
-    report_df_2 = pd.DataFrame(report_list)
+    def generate_report():
+        quadruples = [(gwava_perf, "GWAVA", c1_perf, "C1"), 
+                      (gwava_perf, "GWAVA", c1_ls_perf, "C1+LS"), 
+                      (gwava_perf, "GWAVA", c3_perf, "C3"), 
+                      (c1_perf, "C1", c1_ls_perf, "C1+LS"),
+                      (c1_perf, "C1", c3_perf, "C3"),
+                      (c1_ls_perf, "C1+LS", c3_perf, "C3")]
 
-    report_list = list(t_test_report(c1_ls_perf, "C1+LS", c1_ls_n2v_perf, "C1+LS+N2V", metric_cols))
-    report_df_3 = pd.DataFrame(report_list)
+        for q in quadruples:
+            report_list = list(t_test_report(*q, metric_cols))
+            yield pd.DataFrame(report_list)
 
-    report_df = pd.concat([report_df_1, report_df_2, report_df_3], axis=0)
+
+    report_df = pd.concat(list(generate_report()), axis=0)
     report_df.to_csv("t_test.tsv", sep="\t", index=False)
